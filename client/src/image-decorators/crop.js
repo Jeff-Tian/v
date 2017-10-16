462fc5061c682d5f8e3c07eab377796ba962335b
Jeff Tian@Workstation <jie.tian@live.cn>
2017-10-17 00:24:31 +0800

    Fix v clipped issue

--- client/src/image-decorators/crop.js
+++ client/src/image-decorators/crop.js
@@ -4,8 +4,6 @@ let minResolution = 400;
 
 module.exports = {
     circleCropImageToCanvas: function (img, canvas, context, offsetX, offsetY, scaleX, scaleY) {
-        context.save();
-
         let min = Math.min(img.naturalWidth, img.naturalHeight);
         scaleX = scaleX || img.naturalWidth / img.width;
         scaleY = scaleY || img.naturalHeight / img.height;
@@ -23,6 +21,7 @@ module.exports = {
 
         console.log('cropping with ', offsetX, offsetY);
 
+        context.save();
         let c = shape.drawInscribedCircle(canvas, context);
         context.clip();
         context.drawImage(img, -offsetX * scaleX, -offsetY * scaleY, min, min, 0, 0, canvas.width, canvas.height);
