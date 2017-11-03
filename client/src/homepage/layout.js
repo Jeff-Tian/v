466226191567ec97ba6e8b3da997201898664c9e
Jeff Tian@Workstation <jie.tian@live.cn>
2017-11-03 19:44:07 +0800

    Layout

--- client/src/homepage/layout.js
+++ client/src/homepage/layout.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import App from '../App';
+import '../index.css';
+import {browserHistory, Route, Router} from 'react-router';
+import LoginPage from '../auth/login-page';
+import Orders from '../admin/orders';
+import OrderDetail from '../orders/order-detail';
+import V from '../v/index';
+import Auth from '../auth/auth';
+import NotFound404 from '../errors/404';
+import Settings from '../admin/settings';
+import Config from '../admin/config';
 import {Header, Icon, Menu, Segment, Sidebar, Divider} from 'semantic-ui-react';
 import '../../public/lib/hammer.min';
 import '../../public/lib/hammer-time.min';
-import MainEntryPoint from '../App';
 
-class SidebarLeftScaleDown extends Component {
+function isUserAuthenticated(nextState, replaceState) {
+    if (!Auth.isUserAuthenticated()) {
+        replaceState({
+            nextPathname: nextState.location.pathname
+        }, '/sign-in');
+    }
+}
+
+class Layout extends Component {
     state = {visible: false};
 
     toggleSidebarVisibility = () => this.setState({visible: !this.state.visible});
@@ -41,11 +59,21 @@ class SidebarLeftScaleDown extends Component {
                     </Menu.Item>
                 </Sidebar>
                 <Sidebar.Pusher>
-                    <MainEntryPoint/>
+                    <Router history={browserHistory}>
+                        <Route path="/" component={App}/>
+                        <Route path="/v/:vid" component={V}/>
+                        <Route path="/v/:vid/:orderId" component={V}/>
+                        <Route path="/order/:orderId" component={OrderDetail}/>
+                        <Route path="/sign-in" component={LoginPage}/>
+                        <Route path="/admin/orders" component={Orders} onEnter={isUserAuthenticated}/>
+                        <Route path="/admin/settings" component={Settings} onEnter={isUserAuthenticated}/>
+                        <Route path="/admin/api/config" component={Config} onEnter={isUserAuthenticated}/>
+                        <Route path="/*" component={NotFound404}/>
+                    </Router>
                 </Sidebar.Pusher>
             </Sidebar.Pushable>
         );
     }
 }
 
-export default SidebarLeftScaleDown;
+export default Layout;
--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import {browserHistory, Route, Router} from 'react-router';
-import LoginPage from './auth/login-page';
-import Orders from './admin/orders';
-import OrderDetail from './orders/order-detail';
-import V from './v/index';
-import Auth from './auth/auth';
-import NotFound404 from './errors/404';
-import Settings from './admin/settings';
-import Config from './admin/config';
-import Homepage from './homepage/index';
-
-function isUserAuthenticated(nextState, replaceState) {
-    if (!Auth.isUserAuthenticated()) {
-        replaceState({
-            nextPathname: nextState.location.pathname
-        }, '/sign-in');
-    }
-}
+import Layout from './homepage/layout';
 
 ReactDOM.render((
-    <Router history={browserHistory}>
-        <Route path="/" component={App}/>
-        <Route path="/v/:vid" component={V}/>
-        <Route path="/v/:vid/:orderId" component={V}/>
-        <Route path="/order/:orderId" component={OrderDetail}/>
-        <Route path="/sign-in" component={LoginPage}/>
-        <Route path="/admin/orders" component={Orders} onEnter={isUserAuthenticated}/>
-        <Route path="/admin/settings" component={Settings} onEnter={isUserAuthenticated}/>
-        <Route path="/admin/api/config" component={Config} onEnter={isUserAuthenticated}/>
-        <Route path="/homepage" component={Homepage}/>
-        <Route path="/*" component={NotFound404}/>
-    </Router>
+    <Layout/>
 ), document.getElementById('root'));
