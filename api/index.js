804b946bb8d2afe6fda476c7646d24c45fd6318c
cnjeftia <jeff.tian@outlook.com>
2022-07-13 19:08:47 +0800

    feat: promise fashion

--- api/index.js
+++ api/index.js
@@ -3,4 +3,11 @@ const app = require('../app');
 
 const handler = serverless(app);
 
-export default handler;
+module.exports.handler = async (event, context) => {
+    console.log('event = ', event);
+    console.log('context = ', context);
+    // you can do other things here
+    const result = await handler(event, context);
+    // and here
+    return result;
+};
