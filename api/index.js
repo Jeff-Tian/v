62a6040e3224bc0a29815c62ba476761032bf144
cnjeftia <jeff.tian@outlook.com>
2022-07-14 10:45:28 +0800

    test: log event context string

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ const app = require('../app');
 const handler = serverless(app);
 
 export default async function (event, context) {
-    // console.log('event = ', event);
-    // console.log('context = ', context);
+    console.log('event = ', event.toString());
+    console.log('context = ', context.toString());
     // you can do other things here
     const result = await handler(event, context);
 
