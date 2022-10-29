dfa3d74007e5e6ce355a44233c437aea1d8569a1
cnjeftia <jeff.tian@outlook.com>
2022-10-29 20:30:14 +0800

    fix: toFixed for filename

--- client/src/payment/payment-qr.js
+++ client/src/payment/payment-qr.js
@@ -1,8 +1,8 @@
 import PaymentMethods from "../bll/paymentMethods";
 
 export const getPaymentQrForOrder = (order) => {
-    if(order.paymentMethod === 'wecom-pay') {
-        const fileName = `${order.finalCents / 100}.PNG`
+    if (order.paymentMethod === 'wecom-pay') {
+        const fileName = `${(order.finalCents / 100).toFixed(2)}.PNG`
         return `/images/wecom-pay-qrs/${fileName}`
     } else {
         return (PaymentMethods[order.paymentMethod] && PaymentMethods[order.paymentMethod].receiverImage) || PaymentMethods["wecom-pay"].receiverImage
