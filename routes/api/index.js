346ea3b9792905e978b6a8ab88fe65f563bac19e
cnjeftia <jeff.tian@outlook.com>
2022-10-30 15:33:30 +0800

    feat: add node_env router

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,13 +6,19 @@ const config = require('../../client/src/config');
 const asyncProxy = require('../../common/async-proxy');
 
 router
+    .get('/node_env', async function (ctx, next) {
+        ctx.body = {
+            node_env: process.env.NODE_ENV,
+            config: config
+        }
+    })
     .get('/order/:orderId', async function (ctx, next) {
         ctx.body = await orderBll.get(ctx.params.orderId);
     })
     .get('/orders', async function (ctx, next) {
         ctx.body = await orderBll.list();
     })
-    .post('/orders', async function(ctx, next) {
+    .post('/orders', async function (ctx, next) {
         ctx.body = await orderBll.create(ctx.request.body.type, ctx.request.body.paymentMethod);
     })
 
